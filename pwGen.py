400dd2fdf118b921fce0c26232144557e5a5bb27
harshthakur066 <harshthakur066@gmail.com>
2020-05-06 19:42:49 +0530

    save to file code added

--- pwGen.py
+++ pwGen.py
@@ -1,36 +1,50 @@
-import string 
-import random 
+import string
+import random
 import sys
 
 
 N = int(input("Enter size of string --> "))
+
 counter = int(input("How many password combinations do you need! --> "))
+
+choice = input("Obsfucate the passcode? [Y/n] ")
+
+savef = input("Do you want to save the file? [Y/n]")
+
+if choice == 'n':
+    obs = False
+else:
+    obs = True
 
 
 def run(N):
-    choice = input("Obsfucate the passcode? [Y/n] ")
-    if choice == 'n':
-	    obs = False
-    else:
-	    obs = True
-    
-    res = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation, k = N)) 
+
+    res = ''.join(random.choices(string.ascii_uppercase +
+                                 string.digits + string.ascii_lowercase + string.punctuation, k=N))
     if obs:
-        res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace('l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
+        res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace(
+            'l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
     else:
         pass
-    return print("The generated random string : " +str(res))
+    return res
 
 
-for i in range(counter):
-    run(N)
+def output():
+    if savef == 'n':
+        print("The generated random string : " + str(run(N)))
+    else:
+        sys.stdout = open('pw.txt', 'wt')
+        with open('pw.txt', mode='r+') as file:
+            file.write('The generated random string :')
+            for i in range(counter):
+                file.write('\n' + str(run(N)))
 
 
-# savef = input("Do you want to save the file? [Y/n]")
+try:
+    for i in range(counter):
+        output()
+except:
+    print('Something went wrong')
 
-# if savef == 'n'
-#     pass
-# else:
-#     fname = input("Enter filename -->")
-#     sys.stdout = open(fname,'wt')
-
+finally:
+    print('Done')
