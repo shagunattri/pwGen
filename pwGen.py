2eafbacba987be87a25027bbf06c49f9277c6bd3
shagunattri <shagunattri17@gmail.com>
2020-05-10 18:42:00 +0530

    add ctoc

--- pwGen.py
+++ pwGen.py
@@ -1,21 +1,35 @@
 import string
 import random
 import sys
+import pyperclip
 from pyfiglet import Figlet
-custom_fig = Figlet(font='graffiti')
-print(custom_fig.renderText('pwGen'))
 
-N = int(input("Enter size of string --> "))
-counter = int(input("How many password combinations do you need! --> "))
-choice = input("Obsfucate the passcode? [Y/n] ")
-savef = input("Do you want to save the file? [Y/n]")
+
+custom_ascii = Figlet(font='graffiti')
+print(custom_ascii.renderText('pwGen'))
+
+
+N = int(input("[+] Enter size of string --> "))
+choice = input("[+] Obsfucate the passcode? [Y/n] ")
+savef = input("[+] Do you want to save the file? [Y/n]")
+
+
+def output():
+    if savef == "n":
+        print("[+] The generated random string --> " + str(run(N)))
+    else:
+        filename = input("[+] Input the Filename --> ")
+        sys.stdout = open(filename, 'w',encoding = 'utf-8')
+        with open(filename, mode='r+') as f:
+                f.write(str(run(N)))
+                
+   
 
 if choice == 'n':
     obs = False
 else:
     obs = True
-
-
+    
 def run(N):
     res = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation, k=N))
     if obs:
@@ -25,20 +39,13 @@ def run(N):
     return res
 
 
-def output():
-    if savef == 'n':
-        print("The generated random string : " + str(run(N)))
+def ctoc():
+    if ccinput == 'n':
+        pass
     else:
-        sys.stdout = open('pw.txt', 'w',encoding = 'utf-8')
-        with open('pw.txt', mode='r+') as f:
-            for i in range(counter):
-                f.write("\n" + str(run(N)))
+        print("[+] Password copied to clipboard...")
+        pyperclip.copy()
 
 
-try:
-    for i in range(counter):
-        output()
-except:
-    print('Something went wrong')
-finally:
-    pass
+output()    
+ctoc()
