6656a5502b75096980a17458fc9b464dee33698b
shagunattri <shagunattri17@gmail.com>
2020-05-12 23:35:11 +0530

    remove duplication

--- pwGen.py
+++ pwGen.py
@@ -9,7 +9,6 @@ custom_ascii = Figlet(font='graffiti')
 print(custom_ascii.renderText('pwGen'))
 
 x = True
-
 while x:
     try:
         N = int(input("[+] Enter size of string --> "))
@@ -17,17 +16,16 @@ while x:
     except:
         print('Please enter a valid number')
 
+
 choice = input("[+] Obsfucate the passcode? [Y/n] ")
 savef = input("[+] Do you want to save the file? [Y/n]")
 
-res = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits + string.ascii_lowercase + string.punctuation, k=N))
+res = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation, k=N))
 
 if choice == 'n' or choice == 'N':
     pass
 else:
-    res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace(
-        'l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
+    res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace('l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
 
 
 if savef == 'n' or savef == 'N':
@@ -54,20 +52,3 @@ print ('+------------------------------------+---------------------+')
 print ('| Password                           | Entropy             |')
 print ('+------------------------------------+---------------------+')
 print (' %s                           %s                  ' % (res, entropy))
-
-
-ctoc = input("[+] Copy password to clipborad? [Y/n] ")
-if ctoc == 'n' or ctoc == 'N':
-    pass
-else:
-    pyperclip.copy(res)
-    print("[+] Password copied to clipboard...")
-
-entropy = ((log(82)/log(2)) * len(res))
-
-print('')
-print('+------------------------------------+---------------------+')
-print('| Password                           | Entropy             |')
-print('+------------------------------------+---------------------+')
-print(' %s                           %s                  ' % (res, entropy))
-
