b125e1933504f8d026c70b3838422c6d4f9b1f4d
harshthakur066 <harshthakur066@gmail.com>
2020-05-12 22:39:16 +0530

    bug fixed

--- pwGen.py
+++ pwGen.py
@@ -2,44 +2,57 @@ import string
 import random
 import sys
 import pyperclip
+from math import log
 from pyfiglet import Figlet
-
 
 custom_ascii = Figlet(font='graffiti')
 print(custom_ascii.renderText('pwGen'))
 
+x = True
 
-N = int(input("[+] Enter size of string --> "))
+while x:
+    try:
+        N = int(input("[+] Enter size of string --> "))
+        x = False
+    except:
+        print('Please enter a valid number')
+
 choice = input("[+] Obsfucate the passcode? [Y/n] ")
 savef = input("[+] Do you want to save the file? [Y/n]")
 
-   
-if choice == 'n':
-    obs = False
+
+res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits + string.ascii_lowercase + string.punctuation, k=N))
+
+if choice == 'n' or choice == 'N':
+    pass
 else:
-    obs = True
+    res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace(
+        'l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
 
 
-def output():
-    if savef == "n":
-        print("[+] The generated random string --> " + str(run(N)))
-    else:
-        filename = input("[+] Input the Filename --> ")
-        sys.stdout = open(filename, 'w',encoding = 'utf-8')
-        with open(filename, mode='w') as f:
-                f.write(str(run(N)))
-                f.close()
-                
-
-def run(N):
-    res = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation, k=N))
-    if obs:
-        res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace('l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
-        pyperclip.copy(res)
-    else:
-        pyperclip.copy(res)
-    return res
+if savef == 'n' or savef == 'N':
+    print()
+    print("[+] The generated random string --> " + str(res))
+    print()
+else:
+    filename = input("[+] Input the Filename --> ")
+    with open(filename, mode='w') as f:
+        f.write(res)
+        f.close()
 
 
+ctoc = input("[+] Copy password to clipborad? [Y/n] ")
+if ctoc == 'n' or ctoc == 'N':
+    pass
+else:
+    pyperclip.copy(res)
+    print("[+] Password copied to clipboard...")
 
-output()    
+entropy = ((log(82)/log(2)) * len(res))
+
+print('')
+print('+------------------------------------+---------------------+')
+print('| Password                           | Entropy             |')
+print('+------------------------------------+---------------------+')
+print(' %s                           %s                  ' % (res, entropy))
