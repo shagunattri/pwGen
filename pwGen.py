634c583a9d5c6ba2ee6363dc21bfb620745bdbdb
shagunattri <shagunattri17@gmail.com>
2020-05-12 16:18:37 +0530

    simplify code

--- pwGen.py
+++ pwGen.py
@@ -15,31 +15,25 @@ savef = input("[+] Do you want to save the file? [Y/n]")
 
    
 if choice == 'n':
-    obs = False
+    pass
 else:
-    obs = True
-
-
-def output():
-    if savef == "n":
-        print("[+] The generated random string --> " + str(run(N)))
-    else:
-        filename = input("[+] Input the Filename --> ")
-        sys.stdout = open(filename, 'w',encoding = 'utf-8')
-        with open(filename, mode='w') as f:
-                f.write(str(run(N)))
-                f.close()
-                
-
-def run(N):
     res = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation, k=N))
-    if obs:
-        res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace('l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
-        pyperclip.copy(res)
-    else:
-        pyperclip.copy(res)
-    return res
+    res = res.replace('a', '4').replace('e', '3').replace('i', '!').replace('l', '1').replace('t', '7').replace('s', '$').replace('o', '0')
 
 
 
-output()    
+if savef == 'n':
+    print("[+] The generated random string --> " + str(res))
+else:
+    filename = input("[+] Input the Filename --> ")
+    with open(filename, mode='w') as f:
+        f.write(res)
+        f.close()
+
+
+ctoc = input("[+] Copy password to clipborad? [Y/n] ")
+if ctoc == 'N':
+    pass
+else:
+    pyperclip.copy(res)
+    print("[+] Password copied to clipboard...")   
